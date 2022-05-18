6606ea9aa9a786bbeb62b3e320e2259e556e6078
ant-lazo <ant.14sag@gmail.com>
2022-05-18 01:11:04 -0500

    service-transaction-person

---
+++ src/main/java/com/nttdata/bootcamp/BootcampServiceTransactionPersonApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampServiceTransactionPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampServiceTransactionPersonApplication.class, args);
+	}
+
+}
